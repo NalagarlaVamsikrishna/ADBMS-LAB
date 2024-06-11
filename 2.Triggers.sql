ee7ec787cf9ecd2edb643bdc981206b6a154452f
NalagarlaVamsikrishna <167277199+NalagarlaVamsikrishna@users.noreply.github.com>
2024-06-12 03:25:34 +0530

    Update and rename Triggers.sql to 2.Triggers.sql

--- 2.Triggers.sql
+++ 2.Triggers.sql
@@ -3,6 +3,6 @@ create table student(student_id integer null,name varchar(20),Address varchar(20
 desc student;
 create trigger student_trigger before insert on student for each row set  new.Marks=new.Marks+100;
 insert into student(student_id,name,Address,Marks) values('2','krishna','landon','90');
-select*from student;
 insert into student(student_id,name,Address,Marks) values('3','Ramu','India','70');
+select*from student;
 show triggers;
